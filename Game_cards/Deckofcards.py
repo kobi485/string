2e724bdfd7bfb3cb7e4bc3cabe499d6075a33755
kobi485 <68278560+kobi485@users.noreply.github.com>
2020-08-02 19:17:36 +0300

    Add files via upload

---
+++ Game_cards/Deckofcards.py
@@ -0,0 +1,61 @@
+from Game_cards.Card import Card
+from random import shuffle
+
+
+class Deckofcards:
+
+    # איניט של חפיסת קלפים שלמה עפ הסוגים
+    def __init__(self):
+        self.deck = self._create()
+
+    def _create(self):
+        deck = []
+        for i in range(1, 14):
+            deck.append(Card(i, 0))
+        for i in range(1, 14):
+            deck.append(Card(i, 1))
+        for i in range(1, 14):
+            deck.append(Card(i, 2))
+        for i in range(1, 14):
+            deck.append(Card(i, 3))
+        return deck
+
+    # כתיבה של החפיסה
+    def __str__(self):
+        return (f'i am {self.deck}')
+
+    # ערבוב של החפיסה
+    def _shuffle(self):
+        if len(self.deck) == 52:
+            shuffle(self.deck)
+        else:
+            print('cant shuffle,count the cards!')
+
+    # מחזירה את הקלף הראשון בחבילה
+    def dealOne(self):
+        if len(self.deck) > 0:
+            return self.deck.pop(0)
+
+    # מערבבת את החבילה
+    def newGame(self):
+        self.__init__()
+        self.shuffle()
+        return self.deck[0]
+
+    # מדפיסה את החבילה
+    def show(self):
+        print(self.deck)
+
+#
+# d1 = Deckofcards()
+# print(d1)
+# # d1.shuffle()
+# # print(d1)
+#
+# d1.dealOne()
+# print(d1)
+# #
+# # d1.newGame()
+# print(d1)
+# # d1.show()
+# #
---
+++ Game_cards/Player.py
@@ -0,0 +1,55 @@
+from Game_cards.Deckofcards import Deckofcards
+from Game_cards.Card import Card
+from random import shuffle
+
+
+class Player:
+
+    # הכנסת משתנים של השחקן
+    def __init__(self, name, sum1, numofcards=5):
+        self.list1 = []
+        self.name = ''
+        self.sum1 = sum1
+        self.numofcards = numofcards
+
+    # מקבלת חבילת קלפים חדשה לשחקן
+    def setHand(self, deck1):
+        if self.numofcards <= len(deck1.deck):
+            for i in range(self.numofcards):
+                self.list1.append(deck1.dealOne())
+
+    # מושכת קלף אקראי מהשחקן
+    def getCard(self):
+        if len(self.list1) > 0:
+            shuffle(self.list1)
+            return self.list1.pop(0)
+
+    # מוסיף קלף לשחקן
+    def addCard(self, card1):
+        self.list1.append(card1)
+
+    # מוריד סכום כסף מהשחקן
+    def reduceAmount(self, amount):
+        if type(amount) == int:
+            self.sum1 -= amount
+        else:
+            return 'error,need number'
+
+    # מוסיף כמות כסף לשחקן
+    def addAmount(self, amount):
+        if type(amount) == int:
+            self.sum1 += amount
+        else:
+            return 'error,need number'
+
+    # הדפסה של השחקן עם ערכים
+    def __repr__(self):
+        return f'i am {self.name} and i have {self.sum1}money my cards :{self.list1}'
+
+# d=Deckofcards()
+# d.shuffle()
+# player1=Player('kobi',100,5000)
+# player1.setHand(d)
+# print(player1.list1)
+# # player1.getCard()
+# # player1.addCard(d.deck[0])
